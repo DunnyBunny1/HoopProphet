85bd9f1281721ff78dfdc4d8c7ff13f0a043bc81
DunnyBunny1 <murray.don@northeastern.edu>
2024-01-30 18:40:51 -0500

    refactored project to jupyter notebook files instaed of py files

--- src/mvp_predictor.py
+++
@@ -1,4 +0,0 @@
-def predict_mvp(data):
-    # Placeholder for your MVP prediction logic
-    # Example: Return a hardcoded MVP prediction
-    return "LeBron James"
--- src/scraping_tools/team_data_scraper.py
+++
@@ -1,5 +0,0 @@
-url_template = 'https://www.basketball-reference.com/leagues/NBA_1991_standings.html'
-
-
-def initialize_yearly_team_data(years):
-    pass
